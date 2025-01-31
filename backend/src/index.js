f84f17f07e8dfe4a73d7c7513979cde77983e4bb
Aditya Rana <adityarana.cs@gmail.com>
2025-01-31 21:27:17 +0530

    feat: added support for docker-compose

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,8 @@ import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import healthRoutes from "./routes/health.js";
+
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
@@ -30,7 +32,7 @@ origin: [
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
+app.use("/api/health", healthRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -40,6 +42,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+  console.log("server is running on PORT: " + PORT);
   connectDB();
 });
